43ff219b666503db2d3ab1a8154b176669792603
NurullaWasHere <NurullaWasHere>
2023-02-19 19:26:34 +0600

    db

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,9 +2,9 @@ const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,  process.env.DB_PASSWORD, {
-    host: 'containers-us-west-194.railway.app',
+    host: 'containers-us-west-166.railway.app',
     dialect: 'postgres',
-    port: "5704",
+    port: "5592",
     define: {
         charset: 'utf-8',
         collate: 'utf8_general_ci'
